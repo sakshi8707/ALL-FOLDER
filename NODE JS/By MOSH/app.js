8c061f65e6f900ec60b21429652e0df69cded5b2
Sakshi Tiwari <96986663+sakshi8707@users.noreply.github.com>
2024-07-30 20:45:00 +0530

    Add files via upload

---
+++ NODE JS/By MOSH/app.js
@@ -0,0 +1,10 @@
+
+
+// function sayHello(name)
+// {
+//   console.log('hello '+name);
+
+// }
+
+// sayHello("sakshi");
+console.log(module);
---
+++ NODE JS/By MOSH/logger.js
@@ -0,0 +1,8 @@
+var url = 'http://mylogger.io/log';
+
+
+function log(msg)
+{
+  //send an http request
+  console.log(msg);
+}
